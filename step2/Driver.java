4ae12a88e693e0f7f60da6ae99eb5e029b708728
Connor Morrison <connor.morrison@montana.edu>
2020-02-27 22:52:41 -0700

    Step 2

---
+++ step2/Driver.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import org.antlr.v4.runtime.*;
+
+public class Driver {
+	public static boolean error = false;
+	public static void main(String[] args) throws Exception{
+		try{
+			LITTLELexer lexer = new LITTLELexer(CharStreams.fromFileName(args[0]));
+			CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+			LITTLEParser parser = new LITTLEParser(tokenStream);
+			//parser.removeErrorListeners();
+            BaseErrorListener listener = new BaseErrorListener();
+			parser.addErrorListener(new BaseErrorListener() {
+				@Override
+				public void syntaxError(Recognizer<?,?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+					error = true;
+				}
+			});
+            parser.program();
+			if(error) {
+				System.out.println("Not accepted");
+			} else {
+				System.out.println("Accepted");
+			}
+		} catch (IOException e) {
+			System.out.println("Error occurred: " + e);
+		}
+	}
+}
