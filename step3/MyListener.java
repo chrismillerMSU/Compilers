bcf65afcd4c91bc35a8052d634fe386ec4431500
Christopher <christomill@yahoo.com>
2020-03-21 22:53:38 -0600

    Fuck this shit ass assignment

--- step3/MyListener.java
+++ step3/MyListener.java
@@ -7,33 +7,66 @@ public class MyListener extends LITTLEBaseListener{
     private String currentType;
     private String type;
     private HashMap values;
+    private String lastName;
     private int blockNumber;
 
     MyListener(){
         symbolTableStack = new Stack<HashMap>();
         scopeStack = new Stack<String>();
+        values = new HashMap<String, String>();
         blockNumber = 1;
         type = "";
+        lastName = "";
     }
 
-
+    private void printTable(HashMap symbolTabel){
+        symbolTabel.forEach((k,v) -> {
+            System.out.print("name "+k+" type "+v);
+            if(v == "STRING"){
+                System.out.println(" value " + values.get(k));
+            }else {
+                System.out.println();
+            }
+        });
+    }
 
 
     @Override public void enterProgram(LITTLEParser.ProgramContext ctx) {
         scopeStack.push("GLOBAL");
+        System.out.println("Symbol table GLOBAL");
         symbolTableStack.push(new HashMap<String, String>());
     }
 
     @Override public void exitProgram(LITTLEParser.ProgramContext ctx) {
-        System.out.println("END");
-        System.out.println(symbolTableStack);
+        System.out.println();
+    }
+
+    @Override public void enterIf_stmt(LITTLEParser.If_stmtContext ctx) {
+        scopeStack.push("BLOCK " + blockNumber);
+        System.out.println();
+        System.out.println("Symbol table BLOCK " + blockNumber);
+        symbolTableStack.push(new HashMap<String, String>());
+        blockNumber++;
+    }
+
+    @Override public void enterElse_part(LITTLEParser.Else_partContext ctx) {
+        scopeStack.pop();
+        symbolTableStack.pop();
+        scopeStack.push("BLOCK" + blockNumber);
+        System.out.println();
+        System.out.println("Symbol table BLOCK " + blockNumber);
+        symbolTableStack.push(new HashMap<String, String>());
+        blockNumber++;
+    }
+    @Override public void exitIf_stmt(LITTLEParser.If_stmtContext ctx) {
+        scopeStack.pop();
+        symbolTableStack.pop();
     }
 
     @Override public void enterFunc_declarations(LITTLEParser.Func_declarationsContext ctx) {
-        if(scopeStack.contains("GLOBAL")){
-            System.out.println(scopeStack.pop());
-            System.out.println(symbolTableStack.peek());
-        }
+//        if(scopeStack.contains("GLOBAL")){
+//            System.out.println("Symbol table " + scopeStack.pop());
+//        }
     }
 
     @Override public void enterFunc_decl(LITTLEParser.Func_declContext ctx) {
@@ -42,8 +75,8 @@ public class MyListener extends LITTLEBaseListener{
     }
 
     @Override public void exitFunc_decl(LITTLEParser.Func_declContext ctx) {
-        System.out.println(scopeStack.pop());
-        System.out.println(symbolTableStack.pop());
+        scopeStack.pop();
+        symbolTableStack.pop();
     }
 
     @Override public void enterVar_type(LITTLEParser.Var_typeContext ctx) {
@@ -53,6 +86,14 @@ public class MyListener extends LITTLEBaseListener{
     @Override public void enterString_decl(LITTLEParser.String_declContext ctx) {
         currentType = "STRING";
         type = "str";
+    }
+    @Override public void enterStr(LITTLEParser.StrContext ctx) {
+        System.out.print(" value "+ ctx.getText());
+        System.out.println();
+    }
+
+    @Override public void exitString_decl(LITTLEParser.String_declContext ctx) {
+        type = "";
     }
 
     @Override public void enterParam_decl_list(LITTLEParser.Param_decl_listContext ctx){
@@ -75,9 +116,18 @@ public class MyListener extends LITTLEBaseListener{
         String name = ctx.getText();
         if(type.equals("varDec") || type.equals("str")) {
             HashMap symbolTable = (HashMap) symbolTableStack.peek();
-            symbolTable.putIfAbsent(name, currentType);
+            if(symbolTable.containsKey(name)){
+                //FUCK
+            }
+            symbolTable.put(name, currentType);
+            System.out.print("name "+name+" type "+currentType);
         }else if(type.equals("funct")){
             scopeStack.push(name);
+            System.out.println();
+            System.out.println("Symbol table " + name);
+        }
+        if(type.equals("varDec")){
+            System.out.println();
         }
     }
 }
