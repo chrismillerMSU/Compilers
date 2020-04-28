6d65f86b859461601b1bd52dd179f9e23b92b0a3
Connor Morrison <connor.morrison@montana.edu>
2020-04-28 13:03:01 -0600

    improved the AST

--- step4/ASTNode.java
+++ step4/ASTNode.java
@@ -1,19 +1,33 @@
 class ASTNode {
-	String op;
+	String varName;
 	String type;
 	boolean isExpr;
 	ArrayList<ASTNode> children = new ArrayList<ASTNode>();
-	public ASTNode(String op, String exprType, Boolean isExpr) {
+	
+	/** create an AST node for an expression. i.e. MulExpr. These have no variables, but do have types.
+	*	@param exprType, the type of expression, i.e. Mul, Add, Sub, Div...
+	*	@param isExpr, true if this is an expression.
+	*/
+	public ASTNode(String exprType, Boolean isExpr) {
 		this.type = exprType;
 		this.isExpr = true;
-		this.op = null;
+		this.varName = null;
 	}
-	public ASTNode(String op, String opType) {
+	
+	/** create an AST node for an a variable. i.e. int a, int b, string c.
+	*	@param varName, the name of the variable
+	*	@param varType, the type of variable, i.e. int, float, string...
+	*/
+	public ASTNode(String varName, String varType) {
 		this.type = opType;
-		this.op = op;
+		this.varName = varName;
 		this.isExpr = false;
 	}
-	public void add_op(ASTNode node) {
+	
+	/** add children to ASTNode. This could either be a variable, or an expression.
+	*	@param node, the ASTNode to add.
+	*/
+	public void add_expr(ASTNode node) {
 		this.children.add(node);
 	}
 }
