5623480a3d9e1a19a3f4af250b35a0e579fa0b9a
Connor Morrison <connor.morrison@montana.edu>
2020-04-27 23:32:57 -0600

    Step 4

---
+++ step4/Driver.java
@@ -0,0 +1,33 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.io.IOException;
+
+public class Driver {
+	public static boolean error = false;
+	public static void main(String[] args) throws Exception{
+		try{
+			LITTLELexer lexer = new LITTLELexer(CharStreams.fromFileName(args[0]));
+			CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+			LITTLEParser parser = new LITTLEParser(tokenStream);
+			parser.removeErrorListeners();
+			parser.addErrorListener(new BaseErrorListener() {
+				@Override
+				public void syntaxError(Recognizer<?,?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+					error = true;
+				}
+			});
+			//Step 3
+			MyListener listener = new MyListener();
+			try {
+                new ParseTreeWalker().walk(listener, parser.program());
+            }catch (Error e){
+			    System.out.println(e.getMessage());
+            }
+
+
+		} catch (IOException e) {
+			System.out.println("Error occurred: " + e);
+		}
+	}
+}
---
+++ step4/MyListener.java
@@ -0,0 +1,155 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class MyListener extends LITTLEBaseListener{
+    private Stack symbolTableStack;
+    private Queue<String> scopeOrder;
+    private Queue<String> tokenOrder;
+    private String currentType;
+    private String type;
+    private HashMap values;
+    private String lastName;
+    private int blockNumber;
+    private boolean ifOrWhile;
+
+    MyListener(){
+        symbolTableStack = new Stack<HashMap>();
+        scopeOrder = new LinkedList<>();
+        tokenOrder = new LinkedList<>();
+        values = new HashMap<String, String>();
+        blockNumber = 1;
+        type = "";
+        lastName = "";
+        ifOrWhile = false;
+    }
+
+    private void printTable(HashMap symbolTable){
+        System.out.println("Symbol table "+scopeOrder.remove());
+        while (tokenOrder.size()>0){
+            String name = tokenOrder.remove();
+            String type = (String) symbolTable.get(name);
+            System.out.print("name " + name + " type " + type);
+            if(type.equals("STRING")){
+                System.out.println(" value " + values.get(name));
+            }else{
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+
+
+    @Override public void enterProgram(LITTLEParser.ProgramContext ctx) {
+        scopeOrder.add("GLOBAL");
+        symbolTableStack.push(new HashMap<String, String>());
+    }
+
+    @Override public void exitProgram(LITTLEParser.ProgramContext ctx) {
+        System.out.println();
+    }
+
+    @Override public void enterIf_stmt(LITTLEParser.If_stmtContext ctx) {
+        if(!ifOrWhile) {
+            printTable((HashMap) symbolTableStack.peek());
+        }
+        ifOrWhile = true;
+        scopeOrder.add("BLOCK " + blockNumber);
+        symbolTableStack.push(new HashMap<String, String>());
+        blockNumber++;
+    }
+
+    @Override public void enterElse_part(LITTLEParser.Else_partContext ctx) {
+        if(!ctx.getText().equals("")) {
+            printTable((HashMap) symbolTableStack.pop());
+            scopeOrder.add("BLOCK " + blockNumber);
+            symbolTableStack.push(new HashMap<String, String>());
+            blockNumber++;
+        }
+    }
+    @Override public void exitIf_stmt(LITTLEParser.If_stmtContext ctx) {
+        printTable((HashMap) symbolTableStack.pop());
+    }
+
+    @Override public void enterWhile_stmt(LITTLEParser.While_stmtContext ctx) {
+        if(!ifOrWhile) {
+            printTable((HashMap) symbolTableStack.peek());
+        }
+        ifOrWhile = true;
+        scopeOrder.add("BLOCK " + blockNumber);
+        symbolTableStack.push(new HashMap<String, String>());
+        blockNumber++;
+    }
+
+    @Override public void exitWhile_stmt(LITTLEParser.While_stmtContext ctx) {
+        printTable((HashMap) symbolTableStack.pop());
+    }
+
+    @Override public void enterFunc_declarations(LITTLEParser.Func_declarationsContext ctx) {
+        if(scopeOrder.contains("GLOBAL")){
+            printTable((HashMap) symbolTableStack.peek());
+        }
+
+    }
+
+    @Override public void enterFunc_decl(LITTLEParser.Func_declContext ctx) {
+        type = "funct";
+        symbolTableStack.push(new HashMap<String, String>());
+    }
+
+    @Override public void exitFunc_decl(LITTLEParser.Func_declContext ctx) {
+        if(ifOrWhile) {
+            ifOrWhile = false;
+        }else{
+            printTable((HashMap) symbolTableStack.pop());
+        }
+    }
+
+    @Override public void enterVar_type(LITTLEParser.Var_typeContext ctx) {
+        currentType = ctx.getText();
+    }
+
+    @Override public void enterString_decl(LITTLEParser.String_declContext ctx) {
+        currentType = "STRING";
+        type = "str";
+    }
+    @Override public void enterStr(LITTLEParser.StrContext ctx) {
+        values.put(lastName, ctx.getText());
+    }
+
+    @Override public void exitString_decl(LITTLEParser.String_declContext ctx) {
+        type = "";
+    }
+
+    @Override public void enterParam_decl_list(LITTLEParser.Param_decl_listContext ctx){
+        type = "varDec";
+    }
+
+    @Override public void exitParam_decl_list(LITTLEParser.Param_decl_listContext ctx){
+        type = "";
+    }
+
+    @Override public void enterVar_decl(LITTLEParser.Var_declContext ctx) {
+        type = "varDec";
+    }
+
+    @Override public void exitVar_decl(LITTLEParser.Var_declContext ctx) {
+        type = "";
+    }
+
+    @Override public void enterId(LITTLEParser.IdContext ctx) {
+        String name = ctx.getText();
+        if(type.equals("varDec") || type.equals("str")) {
+            HashMap symbolTable = (HashMap) symbolTableStack.peek();
+            if(symbolTable.containsKey(name)){
+                throw new Error("DECLARATION ERROR " + name);
+            }
+            symbolTable.put(name, currentType);
+            tokenOrder.add(name);
+            lastName = name;
+        }else if(type.equals("funct")){
+            scopeOrder.add(name);
+        }
+    }
+}
